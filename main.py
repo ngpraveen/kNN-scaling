d00b13b3878fb82a67d9ba0c3c46b78456175519
Praveen NG <praveen.ng@gmail.com>
2021-01-22 19:50:52 -0500

    Added comments

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import numpy as np
 import pandas as pd
@@ -12,31 +10,26 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 
 
-# In[2]:
-
-
 n_trials = 100
 n_neighbours = [2, 3, 4, 5, 6]
 
 
-# In[3]:
 
-
-df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+df = pd.read_csv('../heart_failure_clinical_records_dataset.csv')
 X = df.drop('DEATH_EVENT', axis=1)
 y = df['DEATH_EVENT']
 
 
-# In[4]:
+#df.head()
 
-
-df.head()
-
-
-# In[5]:
 
 
 def draw_rand_nums(n_rand):
+    """
+    Returns a list of n_rand random numbers
+    drawn between 0 and 100000. Useful to 
+    create different train and test splits. 
+    """
     rand_nums = []
     for i in range(n_rand):
         r = np.random.randint(1, 100000)
@@ -45,7 +38,10 @@ def draw_rand_nums(n_rand):
 
 
 def make_pipes(k):
-    #n_neighbors = 3
+    """
+    Creates four pipelines and returns them 
+    as in a list. 
+    """
     pipe0 = Pipeline([('KNN           ', KNeighborsClassifier(n_neighbors=k))])
     pipe1 = Pipeline([('MinMaxScaler  ', MinMaxScaler()), ('knn', KNeighborsClassifier(n_neighbors=k))])
     pipe2 = Pipeline([('RobustScaler  ', RobustScaler()), ('knn', KNeighborsClassifier(n_neighbors=k))])
@@ -55,11 +51,11 @@ def make_pipes(k):
     return pipes
 
 def get_scores(pipes, display=False):
-    '''
+    """"
     Finds score for each pipeline in 'pipes' list.
-    Returns a list of scores.
-    If display=True, each score is also printed.
-    '''
+    Returns a list of scores. If display=True, 
+    each score is also printed to the screen.
+    """
     scores = []
     for p in pipes:
         p.fit(X_train, y_train)
@@ -72,8 +68,24 @@ def get_scores(pipes, display=False):
     return scores
 
 
-# In[9]:
 
+"""
+1. n_trials random numbers are created. 
+2. For each n_neighbours value (number of neighbors=k)
+   train-test split is performed.
+3. Each model (kNN alone, kNN with different scalings),
+   model is trained and tested using Pipelines. 
+4. Scores are stored in a list. 
+
+The random numbers are used as random seed while 
+splitting data into train and test data sets. 
+Please note, cross validation is not done even though 
+it is better and more efficient. 
+
+In this version, train-test split is done inside 
+the "k in n_neighbors" loop. This is not optimal.
+But for a small data set, this works fine. 
+"""
 
 rand_nums = draw_rand_nums(n_trials)
 scores_df = pd.DataFrame()
@@ -87,29 +99,11 @@ for k in n_neighbours:
         scores.append(get_scores(pipes))
 
     scores_df["k="+str(k)] = pd.DataFrame(scores).mean()
-#    print("k = ", k)
-#    print(pd.DataFrame(scores).mean())
+
+scores_df.index = ["pipe=" + str(i) for i in scores_df.index ]
+scores_df.style.set_caption("Average Score")
 
 
-# In[10]:
-
-
-scores_df
-
-
-# In[8]:
-
-
-scores_df.T.max()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
